b1d1799bc69dd5283a010c95d584adf20aedefa0
Curtis Scott <curtis.scott@accenturefederal.com>
2017-10-19 09:04:10 -0400

    v4 booleans

--- script.js
+++ script.js
@@ -1,18 +1,26 @@
-var todosObj = {
-  todos: ['task 1', 'task 2', 'task 3'],
+var todoList = {
+  todos: [],
   displayTodos: function(){
     console.log(this.todos); 
   },
-  addTodo: function(todo){
-    this.todos.push(todo);
+  addTodo: function(todoText){
+    this.todos.push({
+      todoText: todoText,
+      completed: false
+    });
     this.displayTodos();
   },
   changeTodo: function(idx, newVal){
-    this.todos[idx] = newVal;
+    this.todos[idx].todoText = newVal;
     this.displayTodos();
   },
   deleteTodo: function(idx){
     this.todos.splice(idx, 1);
+    this.displayTodos();
+  },
+  toggleCompleted: function(idx){
+    var todo = this.todos[idx];
+    todo.completed = !todo.completed;
     this.displayTodos();
   }
 };
